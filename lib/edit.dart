cbe84b96ece4a2f2913f34096aefd284dfa02ba6
aseel-uni <442200868@student.ksu.edu.sa>
2024-02-27 05:29:40 +0300

    added edit page

---
+++ lib/edit.dart
@@ -0,0 +1,138 @@
+import 'package:flutter/material.dart';
+
+class Edit extends StatefulWidget {
+  
+ const Edit ({super.key });
+ @override
+ State<Edit> createState() => _EditState();
+} 
+class _EditState extends State<Edit> {
+  TextEditingController username = TextEditingController();
+  TextEditingController password = TextEditingController();
+  @override
+  void initState() {
+    username.text = ""; //innitail value of text field
+    password.text = "";
+    super.initState();
+  }
+  @override
+  Widget build(BuildContext context) {
+   return  Scaffold(
+          appBar: AppBar(  
+           centerTitle: true,
+            iconTheme: IconThemeData(color: Color(0xFF6869D6) , size: 33 ),
+            title: Text("تعديل الملف الشخصي" ,style: TextStyle(fontSize: 36 , color: Color(0xFF6869D6) , fontWeight: FontWeight.bold) )
+        ),
+          body: SingleChildScrollView(
+            padding:EdgeInsets.symmetric( horizontal: 10) ,
+            child:Column(
+              mainAxisAlignment: MainAxisAlignment.start,
+              crossAxisAlignment: CrossAxisAlignment.end,
+              children: [
+              const SizedBox(height: 50),
+              const Row(children: [Expanded(child: 
+               Text("الأسم" , 
+                textAlign:TextAlign.right  ,
+                style: TextStyle(fontSize: 35 , color: Colors.black54,fontWeight: FontWeight.w100))),],),
+              const SizedBox(height: 10),
+              TextField(
+                keyboardType: TextInputType.name,
+                textAlign: TextAlign.right,
+                decoration: InputDecoration(
+                  hintText: "محمد",
+                  hintStyle: TextStyle(fontSize: 26 ,color: Color(0xFFA6A6A6) ,fontWeight: FontWeight.w100),
+                  prefixIcon: Icon(Icons.edit, size: 35.0,),
+                  prefixIconColor: Color(0xFF4C4D7B),
+                  border: OutlineInputBorder(
+                    borderSide:const BorderSide(
+                      color:Color(0xFFA6A6A6),
+                      width: 2.0),
+                    borderRadius: BorderRadius.circular(70)
+                   ),
+                  isDense: true,
+                  contentPadding: EdgeInsets.all(10)
+                  ),
+                textDirection: TextDirection.rtl,
+              ),
+              const SizedBox(height: 20),
+              const Row(children: [Expanded(child: 
+               Text("البريد الالكتروني" , 
+                textAlign:TextAlign.right  ,
+                style: TextStyle(fontSize: 35 , color: Colors.black54,fontWeight: FontWeight.w100))),],),
+              const SizedBox(height: 10),
+              TextField(
+                keyboardType: TextInputType.emailAddress,
+                textAlign: TextAlign.right,
+                decoration: InputDecoration(
+                  hintText: "moh@gmail.com",
+                  hintStyle: TextStyle(fontSize: 26 ,color: Color(0xFFA6A6A6) ,fontWeight: FontWeight.w100),
+                  prefixIcon: Icon(Icons.edit
+                  ,size: 35.0,),
+                  prefixIconColor: Color(0xFF4C4D7B),
+                  border: OutlineInputBorder(
+                    borderSide:const BorderSide(
+                      color:Color(0xFFA6A6A6),
+                      width: 2.0),
+                    borderRadius: BorderRadius.circular(70)
+                   ),
+                  isDense: true,
+                  contentPadding: EdgeInsets.all(10)
+                  ),
+                textDirection: TextDirection.rtl,
+              ),
+              const SizedBox(height: 20),
+              const Row(children: [Expanded(child: 
+               Text("كلمة المرور", 
+                textAlign:TextAlign.right  ,
+                style: TextStyle(fontSize: 35 , color: Colors.black54,fontWeight: FontWeight.w100))),],),
+              const SizedBox(height: 10),
+              TextField(
+                obscureText: true,
+                textAlign: TextAlign.right,
+                decoration: InputDecoration(
+                  hintText: "*******",
+                  hintStyle: TextStyle(fontSize: 26,color: Color(0xFFA6A6A6) ,fontWeight: FontWeight.w100),
+                  prefixIcon: Icon(Icons.edit
+                  ,size: 35.0,),
+                  prefixIconColor: Color(0xFF4C4D7B),
+                  border: OutlineInputBorder(
+                    borderSide:const BorderSide(
+                      color:Color(0xFFA6A6A6),
+                      width: 2.0),
+                    borderRadius: BorderRadius.circular(70)
+                   ),
+                  isDense: true,
+                  contentPadding: EdgeInsets.all(10)
+                  ),
+                textDirection:TextDirection.rtl,
+              ),
+              const SizedBox(height: 70),
+              Align(
+                alignment: Alignment.center,
+                child: MaterialButton(
+                  onPressed: () {
+                    
+                  },
+                  color: Color(0xFF6869D6),
+                  textColor: Colors.white,
+                  padding: const EdgeInsets.symmetric(
+                    vertical: 11,
+                    horizontal: 100,
+                  ),
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(30),
+                  ),
+                  child:const Text(
+                    'تعديل',
+                    style: TextStyle(
+                      fontSize: 30,
+                    ),
+                  ),
+                ),
+              ),
+      ],
+     )
+    )
+   );
+  }
+}
--- lib/settings.dart
+++ lib/settings.dart
@@ -2,7 +2,7 @@
 
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
-
+import 'package:hello_world/edit.dart';
 class settings extends StatefulWidget {
   const settings({Key? key}) : super(key: key);
 
@@ -15,83 +15,85 @@ class _settingsState extends State<settings> {
  
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        centerTitle: true,
-        iconTheme: IconThemeData(color: Color(0xFF6869D6) , size: 33 ),
-        title: Text("الأعدادات" ,style: TextStyle(fontSize: 36 , color: Color(0xFF6869D6) , fontWeight: FontWeight.bold) )
-        ),
-      body: Container(
-        
-        margin: EdgeInsets.symmetric(vertical : 10 , horizontal: 10), 
-        child : ListView(
-        children: [
-          Expanded(
-            flex: 1,
-            child: Container(
-        width: 500,
-        height: 450,
-        //color: Colors.lightBlue,
-        margin: EdgeInsets.symmetric(vertical : 80 , horizontal: 10), 
-        child : Column(
-          crossAxisAlignment: CrossAxisAlignment.end,
-          children: [ const 
-          Text("..تطبيق عون \n يوفر لك وسائل مطورة نحو حياة أفضل" ,
-             textAlign:TextAlign.right  , 
-             style: TextStyle(
-              fontSize: 26 , 
-              color: Color(0xFFA6A6A6),
-              fontWeight: FontWeight.w100)
-              ),
-          Container(height:40 ,),
-          TextButton(
-            onPressed: () {
-              //Navigator.of(context).push(MaterialPageRoute
-                   // (builder: (context) => Edit()));
-            },
-            style: ElevatedButton.styleFrom(
-                primary: Colors.white, ),
-            child: const Row(
-               
-               children: [
-                Icon(Icons.person,
-                size: 40.0,
-                color: Colors.black,),
-               Text('تعديل الملف الشخصي' , style: TextStyle(color: Colors.black , fontSize:30 ),), // <-- Text
-               SizedBox( width: 4,),
-               ],),)
-          ],),),
-          ),
-          Expanded(
-            flex: 1,
-            
-            child: Row(
-            children: [TextButton(
-
-            onPressed: () async {
-              await FirebaseAuth.instance.signOut();
-              Navigator.of(context).pushNamedAndRemoveUntil("logIn", (route) => false);
-
-
-
-              
-            },
-            style: ElevatedButton.styleFrom(
-                primary: Colors.white, ),
-            child: const Row(
-               mainAxisAlignment: MainAxisAlignment.end,
-               mainAxisSize: MainAxisSize.min,
-               children: [
-                Icon(Icons.logout_rounded,
-                size: 30.0,
-                color: Colors.black,),
-               Text("تسجيل الخروج", style: TextStyle(color: Colors.black , fontSize:25 ),), // <-- Text
-               
-               ],),)
-              //TextButton.icon(onPressed: (){}, label: Text("تسجيل الخروج" , ) , icon: Icon(Icons.power_settings_new_outlined), )
-            ],
-          ))
-      ]))
-    );
+     return Scaffold(
+        appBar: AppBar(
+            centerTitle: true,
+            iconTheme: IconThemeData(color: Color(0xFF6869D6), size: 33),
+            title: Text("الأعدادات",
+                style: TextStyle(
+                    fontSize: 36,
+                    color: Color(0xFF6869D6),
+                    fontWeight: FontWeight.bold))),
+        body: Container(
+            margin: EdgeInsets.symmetric(vertical: 10, horizontal: 10),
+            child: Column(children: [
+              Container(
+                  width: 500,
+                  height: 450,
+                  margin: EdgeInsets.symmetric(vertical: 80, horizontal: 10),
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.end,
+                    children: [
+                      const Text(
+                          "..تطبيق عون \n يوفر لك وسائل مطورة نحو حياة أفضل",
+                          textAlign: TextAlign.right,
+                          style: TextStyle(
+                              fontSize: 26,
+                              color: Color(0xFFA6A6A6),
+                              fontWeight: FontWeight.w100)),
+                      Container(
+                        height: 40,
+                      ),
+                      TextButton(
+                        onPressed: () {
+                          Navigator.of(context).push(
+                              MaterialPageRoute(builder: (context) => Edit()));
+                        },
+                        style: ElevatedButton.styleFrom(
+                          primary: Colors.white,
+                        ),
+                        child: const Row(
+                          mainAxisAlignment: MainAxisAlignment.end,
+                          mainAxisSize: MainAxisSize.min,
+                          children: [
+                            Text(
+                              'تعديل الملف الشخصي',
+                              textAlign: TextAlign.right,
+                              style:
+                                  TextStyle(color: Colors.black, fontSize: 30),
+                            ), // <-- Text
+                            SizedBox(
+                              width: 4,
+                            ),
+                            Icon(
+                              Icons.person,
+                              size: 40.0,
+                              color: Colors.black,
+                            ),
+                          ],
+                        ),
+                      )
+                    ],
+                  )),
+              Expanded(
+                  child: Align(
+                alignment: Alignment.bottomRight,
+                child: ElevatedButton(
+                  onPressed: () {},
+                  style: ElevatedButton.styleFrom(
+                    primary: Colors.white,
+                  ),
+                  child: Row(mainAxisSize: MainAxisSize.min, children: const [
+                    Text('تسجيل الخروج',
+                        style: TextStyle(color: Colors.black, fontSize: 25)),
+                    Icon(
+                      Icons.logout_rounded,
+                      color: Colors.black,
+                      size: 30.0,
+                    ),
+                  ]),
+                ),
+              ))
+            ])));
   }
 }
